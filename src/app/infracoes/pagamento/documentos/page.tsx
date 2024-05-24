080df167c6615a6097d9e527a686939eb142b204
Rafael Silverio <fael_st@hotmail.com>
2024-05-24 17:12:09 -0300

    correct deploy

--- src/app/infracoes/pagamento/documentos/page.tsx
+++ src/app/infracoes/pagamento/documentos/page.tsx
@@ -1,8 +1,8 @@
-import { CircularProgress } from '@mui/material';
 import dynamic from 'next/dynamic';
+import { LoadingSpinner } from '../../../../components/LoadingSpinner';
 
 const DynamicDocumentsPage = dynamic(() => import('@/components/pages/documents'), {
-  loading: () => <LoadingSpinner /> => <CircularProgress color="error" />
+  loading: () => <LoadingSpinner />
 });
 
 export default function Infracoes() {
